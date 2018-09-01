daf295ea3a963a04ec166a9f53c3437e69a5fa24
MarlenaFlueh <mf@esyoil.com>
2018-09-01 13:40:03 +0200

    make get method work

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,16 @@ const mongoose = require("mongoose");
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "it's so cool this get req!"
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.post("/", (req, res, next) => {
@@ -20,12 +27,15 @@ router.post("/", (req, res, next) => {
     .save()
     .then(resutl => {
       console.log(result);
+      res.status(200).json({
+        message: "it's so cool this post req!",
+        createdProduct: product
+      });
     })
-    .catch(err => console.log(err));
-  res.status(200).json({
-    message: "it's so cool this post req!",
-    createdProduct: product
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 // get single product
