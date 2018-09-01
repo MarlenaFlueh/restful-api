05862d36a153ab33adcbfcd0e6f7940bf8ba81fc
MarlenaFlueh <mf@esyoil.com>
2018-09-01 13:01:22 +0200

    add bodyparser and access

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const app = express();
 const productRoutes = require("./api/routes/products");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, DELETE, POST, GET, PATCH");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/products", productRoutes);
 
